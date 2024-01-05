8b1484bdf384d5b1502aae18bd0d498f37cb74dc
additya07 <adithya.nakka07@gmail.com>
2024-01-05 17:42:25 +0530

    Updated as SPA

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function renderToChats() {
+    window.location.href = 'index.html'
+}
+function renderToStatus() {
+    window.location.href = 'status.html'
+}
+function renderToChats() {
+    window.location.href = 'calls.html'
+}
